bb11c550e8df3ac71f91e0226ddc73e4c4012c51
Daniil Brunov <danilabrunov@gmail.com>
2025-08-26 14:12:53 +0300

    writing dictionares

--- excel_manager.py
+++ excel_manager.py
@@ -1,11 +1,15 @@
 # loading, saving, crud
 
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 import datetime as datetime 
 
 EXCEL_FILE = "data.xls"
 
-COLUMNS = []
+COLUMNS = ["Процедура", "Пояснение", "Формат", "Наименование поля", "Номер поля", "Публичный синоним", "grant", "Статус разработки"]
+
+STATUS_COLORS = {"Архив": "FFC7CE", "Готово к внедрению": "FFEB9C", "Используется": "C6EFCE"}
 
 def load_data():
     pass
